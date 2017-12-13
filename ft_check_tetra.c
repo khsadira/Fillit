ba12468435ed1e92ffb968c204dc4f1b1b3ec805
Simon CHAKOR <schakor@e1r2p7.42.fr>
2017-12-13 13:23:22 +0100

    add check

---
+++ ft_check_tetra.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_tetra.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schakor <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/13 11:17:34 by schakor           #+#    #+#             */
+/*   Updated: 2017/12/13 12:32:57 by schakor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+#include <stdio.h>
+
+int		ft_check_tetra(t_uint16 *tetra, int	nb_tetra)
+{
+	int	i;
+
+	i = 0;
+	printf("\n\nCHECK TETRA\n\n");
+	while (i <= nb_tetra)
+	{
+		printf("tetra[%d] = %#x\n", i, tetra[i]);
+		if (tetra[i] != 0xf000 && tetra[i] != 0x8888 && tetra[i] != 0xcc00 && tetra[i] != 0x4c40 &&
+			tetra[i] != 0x8c80 && tetra[i] != 0xe400 && tetra[i] != 0x4e00 && tetra[i] != 0x44c0 &&
+			tetra[i] != 0x8e00 && tetra[i] != 0xe200 && tetra[i] != 0xc880 && tetra[i] != 0x88c0 &&
+			tetra[i] != 0xe800 && tetra[i] != 0xc440 && tetra[i] != 0x2e00 && tetra[i] != 0x6c00 &&
+			tetra[i] != 0x8c40 && tetra[i] != 0xc600 && tetra[i] != 0x4c80)
+			return (0);
+		i++;
+	}
+	return (1);
+}
