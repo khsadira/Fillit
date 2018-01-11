6846177a9b654fc9c575eeab22f2c20526acdb91
Khan SADIRAC <khsadira@e1r10p23.42.fr>
2018-01-11 14:50:39 +0100

    header and makefile update

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: schakor <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/11/23 18:14:23 by schakor           #+#    #+#              #
-#    Updated: 2018/01/09 13:47:15 by schakor          ###   ########.fr        #
+#    Updated: 2018/01/11 11:01:35 by khsadira         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ CC		=	gcc
 
 CFLAGS	=	#-Wall -Werror -Wextra
 
-SRC		=	main.c ft_read_file.c ft_treat_file.c ft_check_tetra.c ft_place_tetra.c
+SRC		=	main.c ft_read_file.c ft_treat_file.c ft_check_tetra.c ft_place.c ft_print_map.c
 
 OBJ		=	$(SRC:.c=.o)
 
--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: schakor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 18:18:38 by schakor           #+#    #+#             */
-/*   Updated: 2018/01/09 13:46:18 by schakor          ###   ########.fr       */
+/*   Updated: 2018/01/11 11:29:23 by khsadira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ typedef unsigned int		t_uint32;
 char		*ft_read_file(char *file);
 void		ft_treat_file(char *content, t_uint16 **ret);
 int			ft_check_tetra(t_uint16 *tetra, int nb_tetra);
-void		ft_solve(t_uint16 *tetra, int nb_tetra);
+void		ft_solve(t_uint16 *map, t_uint16 *tetra, int nb_tetra);
+void		ft_print_map(t_uint16 *map);
 
 #endif
