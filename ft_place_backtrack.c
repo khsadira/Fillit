21ea25c0fb2ca8da9bf4db92beda292c1e37d1b6
Khan SADIRAC <khsadira@e2r3p6.42.fr>
2018-01-24 12:12:21 +0100

    backtracking work correctly

--- ft_place_backtrack.c
+++ ft_place_backtrack.c
@@ -6,7 +6,7 @@
 /*   By: schakor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/10 14:16:24 by schakor           #+#    #+#             */
-/*   Updated: 2018/01/11 11:40:21 by khsadira         ###   ########.fr       */
+/*   Updated: 2018/01/24 12:11:44 by khsadira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ static int  ft_is_placable(t_uint16 tetra, t_uint16 *map, int n, int position)
 	int     col;
 
 	line = position / 16;
+	col = position % 16;
 	while (line < n)
 	{
-		col = position % 16;
 		while (col < n)
 		{
 			if (((((tetra & 0xF000) >> col) & (map)[line]) == 0) &&
@@ -30,6 +30,7 @@ static int  ft_is_placable(t_uint16 tetra, t_uint16 *map, int n, int position)
 				return (line * 16 + col);
 			col++;
 		}
+		col = 0;
 		line++;
 	}
 	return (-1);
@@ -45,16 +46,6 @@ static void	ft_create_border(t_uint16 *map, int n)
 	(map)[i] = 0xFFFF;
 }
 
-static void	ft_delete_border(t_uint16 *map, int n)
-{
-	int		i;
-
-	i = 0;
-	while (i < n)
-		(map)[i++] ^= 1 << (15 - n);
-	(map)[i] = 0x0000;
-}
-
 static void	ft_place(t_uint16 tetra, t_uint16 *map, int row, int col)
 {
 	(map)[row] ^= (tetra & 0xF000) >> col;
@@ -67,49 +58,46 @@ void	ft_solve(t_uint16 *map, t_uint16 *tetra, int nb_tetra)
 {
 	int		i;
 	int		square;
-	int		position;
-	int		bposition;
+	int		position[26];
 
-	square = ft_sqrt(nb_tetra*2*2);
-	position = 0;
+	square = ft_sqrt(nb_tetra*4);
 	i = 0;
 	while (i < 16)
-		(map)[i++] = 0;
+		position[i++] = 0;
+	i = 0;
+	while (i < 16)
+		map[i++] = 0;
 	i = 0;
 	ft_create_border(map, square);
-	ft_print_map(map);
-	printf("\nsquare = %d\n", square);
 	while (i < nb_tetra)
 	{
-		position = ft_is_placable(tetra[i], map, square, position);
-		if ((position) == -1 && i == 0)
+		position[i] = ft_is_placable(tetra[i], map, square, position[i]);
+		if (position[i] == -1)
 		{
-			printf("square = %d\n", square);
-			while (i < 16)
-				map[i++] = 0;
-			i = 0;
-			square++;
-			ft_create_border(map, square);
-			position = 0;
+			if (i == 0)
+			{
+				while (i < 16)
+					map[i++] = 0;
+				i = 0;
+				square++;
+				ft_create_border(map, square);
+				while (i < 16)
+					position[i++] = 0;
+				i = 0;
+			}
+			else
+			{
+				i--;
+				ft_place(tetra[i], map, position[i] / 16, position[i] % 16);
+				position[i]++;
+			}
 		}
-		else if (position == -1)
+		else
 		{
-			printf("i = %d, position = %d\n", i, position);
-			i--;
-			ft_place(tetra[i], map, bposition / 16, bposition % 16);
-			position = bposition + 1;
-		}
-		else if ((position) != -1)
-		{
-			printf("position = %d\n", position);
-			ft_place(tetra[i], map, position / 16, position % 16);
+			ft_place(tetra[i], map, position[i] / 16, position[i] % 16);
+			ft_putchar(10);
 			i++;
-			bposition = position;
-			position = 0;
+			position[i] = 0;
 		}
 	}
-	printf("\nnb tetra = %d\n",nb_tetra);
-	ft_putstr("Nous avons un carre de : ");
-	ft_putnbr(square);
-	ft_putchar(10);
 }
